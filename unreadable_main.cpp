1731a9028d5e890adc4097c4443246ea2ee62c27
rezch <krudanya1008@gmail.com>
2024-04-27 20:57:22 +0300

    add unreadable main template

--- unreadable_main.cpp
+++ unreadable_main.cpp
@@ -70,38 +70,38 @@ template<class T>bool(set_max)(T&a,T(b)){{if(a\
 >=b)return(false);}{a=b;return(true);}}templat\
 e<class T>bool(set_min)(T&a,T(b)){{if(a<=b)ret\
 urn(false);}{a=b;return(true);}}template<class\
- T,class U>T(ceil)(T(x),U(y)){return(x>0?(x+y-\
-1)/y:x/y);}template<class T,class U>T(floor)(T\
-(x),U(y)){return(x>0?x/y:(x-y+1)/y);}int(topbi\
-t)(int(x)){return(x==0?-1:31-__builtin_clz(x))\
-;}int(lowbit)(int(x)){return(x==0?-1:__builtin\
-_ctz(x));}int(popcnt)(int(x)){return __builtin\
-_popcount(x);}template<class T,class U>using u\
-mtt=std::unordered_map<T,U>;template<class T>u\
-sing vvt=std::vector<std::vector<T>>;template<\
-class T,class U>using ptt=std::pair<T,U>;templ\
-ate<class T>using umt=std::unordered_map<T,T>;\
-template<class T>using mst=std::multiset<T>;te\
-mplate<class T>using pt=std::pair<T,T>;templat\
-e<class T>using vt=std::vector<T>;template<cla\
-ss T>using stk=std::stack<T>;template<class T>\
-using qt=std::queue<T>;template<class T>using \
-st=std::set<T>;typedef std::string(str);typede\
-f vt<pt<int>>(vpi);typedef vvt<int>(vvi);typed\
-ef vt<int>(vi);typedef pt<int>(pi);void(read)(\
-){};template<class T>void(read_)(T&value){std:\
-:cin>>value;}template<class T,class...U>void(r\
-ead)(T&head,U&...tail){read_(head);read(tail..\
-.);}void(write)(){std::cout<<std::endl;};templ\
-ate<class T> void(write_)(T(value)){std::cout<\
-<value<<' ';}template<class T,class...U>void(w\
-rite)(T(head),U...tail){write_(head);write(tai\
-l...);}template<class T>void(set_)(int(n),T(va\
-lue)){};template<class T,class U>void(_set)(in\
-t(n),T(value),U&list){list.assign(n,value);};t\
-emplate<class T,class U,class...W>void(set_)(i\
-nt(n),T(value),U&head,W&...tail){_set(n,value,\
-head);set_(n,value,tail...);};
+ T,class U>T(ceil)(T(x),U(y)){return((x)>(0)?(\
+x+y-1)/(y):(x)/y);}template<class T,class U>T(\
+floor)(T(x),U(y)){return(x>0?x/y:(x-y+1)/y);}i\
+nt(topbit)(int(x)){return(x==0?-1:(31)-__built\
+in_clz(x));}int(lowbit)(int(x)){return(x==0?-1\
+:__builtin_ctz(x));}int(popcnt)(int(x)){return\
+(__builtin_popcount(x));}template<class T,clas\
+s U>using umtt=std::unordered_map<T,U>;templat\
+e<class T>using vvt=std::vector<std::vector<T>\
+>;template<class T,class U>using ptt=std::pair\
+<T,U>;template<class T>using umt=std::unordere\
+d_map<T,T>;template<class T>using mst=std::mul\
+tiset<T>;template<class T>using pt=std::pair<T\
+,T>;template<class T>using vt=std::vector<T>;t\
+emplate<class T>using stk=std::stack<T>;templa\
+te<class T>using qt=std::queue<T>;template<cla\
+ss T>using st=std::set<T>;typedef std::string(\
+str);typedef vt<pt<int>>(vpi);typedef vvt<int>\
+(vvi);typedef vt<int>(vi);typedef pt<int>(pi);\
+void(read)(){ };template<class T>void(read_)(T\
+&(value)){std::cin>>value;}template<class T,cl\
+ass...U>void(read)(T&head,U&...tail){read_(hea\
+d);read(tail...);}void(write)( ){std::cout<<st\
+d::endl;};template<class T> void(write_)(T(val\
+ue)){std::cout<<value<<' ';}template<class T,c\
+lass...U>void(write)(T(head),U...tail){write_(\
+head);write(tail...);}template<class T>void(se\
+t_)(int(n),T(value)){ };template<class T,class\
+ U>void(_set)(int(n),T(value),U&list){list.ass\
+ign(n,value);};template<class T,class U,class.\
+..W>void(set_)(int(n),T(value),U&head,W&...tai\
+l){_set(n,value,head);set_(n,value,tail...);};\
 
 // ------------------ SOLVE ------------------
 
