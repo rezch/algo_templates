c56239816e3107b074c87a3a0f649c19abc043a0
rezch <krudanya1008@gmail.com>
2024-04-29 12:27:50 +0300

    main upd write print

--- main_template.cpp
+++ main_template.cpp
@@ -24,6 +24,8 @@
 #define fast std::cin.tie(0); \
         std::cout.tie(0); \
         std::ios_base::sync_with_stdio(0);
+#define rrng(var, from, to) \
+        for (int var = from; var >= to; --var)
 #define arng(var, from, to) \
         for (int var = from; var < to; ++var)
 #define mrng(vara, varb, ind1, ind2) \
@@ -32,17 +34,7 @@
         for (auto& var : list)
 #define rng(var, to) \
         arng(var, 0, to)
-#define asrng(var, from, to, step) \
-        for (int var = from; (-1 + 2 * int(step > 0)) * var < (-1 + 2 * int(step > 0)) * to; var += step)
-#define srng(var, to, step) \
-        for (int var = 0; (-1 + 2 * int(step > 0)) * var < (-1 + 2 * int(step > 0)) * to; var += step)
-#define print(list) \
-        iter(var, list) { std::cout << var << ' '; } std::cout << std::endl;
-#define prints(list, ostream) \
-        iter(var, list) { ostream << var << ' '; } ostream << std::endl;
 #define clr(list, size) std::memset(&list, 0, sizeof(list[0]) * size);
-#define inputs(list, istream) iter(var, list) { istream >> var; }
-#define rsort_(list) std::sort(list.rbegin(), list.rend())
 #define input(list) iter(var, list) { std::cin >> var; }
 #define precision(n) std::fixed << std::setprecision(n)
 #define sort_(list) std::sort(list.begin(), list.end())
@@ -63,14 +55,18 @@
 #define pb push_back
 #define in std::cin
 
-template <class T> bool set_max(T& a, T b) { { if (a >= b) return false; } { a = b; return true; } }
-template <class T> bool set_min(T& a, T b) { { if (a <= b) return false; } { a = b; return true; } }
+template <class T> bool set_max(T& a, T b)
+{ { if (a >= b) return false; } { a = b; return true; } }
+template <class T> bool set_min(T& a, T b)
+{ { if (a <= b) return false; } { a = b; return true; } }
 
-template <typename T, typename U> T ceil(T x, U y) { return (x > 0 ? (x + y - 1) / y : x / y); }
-template <typename T, typename U> T floor(T x, U y) { return (x > 0 ? x / y : (x - y + 1) / y); }
 int topbit(int x) { return (x == 0 ? -1 : 31 - __builtin_clz(x)); } // 0 -> -1
 int lowbit(int x) { return (x == 0 ? -1 : __builtin_ctz(x)); } // 0 -> -1
 int popcnt(int x) { return __builtin_popcount(x); }
+template <typename T, typename U> T ceil(T x, U y)
+{ return (x > 0 ? (x + y - 1) / y : x / y); }
+template <typename T, typename U> T floor(T x, U y)
+{ return (x > 0 ? x / y : (x - y + 1) / y); }
 
 template <class T, class U> using umtt = std::unordered_map<T, U>;
 template <class T> using vvt = std::vector<std::vector<T>>;
@@ -88,18 +84,23 @@ typedef vvt<int> vvi;
 typedef vt<int> vi;
 typedef pt<int> pi;
 
-void read() { };
+template <class T> void print(T& list, char splitter = ' ')
+{ for (const auto& value : list) { std::cout << value << splitter; } }
+
+void read() { }
 template <class T> void read_(T& value) { std::cin >> value; }
 template <class T, class... U> void read(T& head, U&... tail) { read_(head); read(tail...); }
 
-void write() { std::cout << std::endl; };
-template <class T> void write_(T value) { std::cout << value << ' '; }
-template <class T, class... U> void write(T head, U... tail) { write_(head); write(tail...); }
+void write_() { std::cout << std::endl; }
+void write() { std::cout << std::endl; }
+template <class T> void W (T value) { std::cout << ' ' << value; }
+template <class T, class...U> void write_(T head, U... tail) { W(head); write_(tail...);}
+template <class T, class...U> void write(T head, U... tail) { std::cout << head; write_(tail...); }
 
-template <class T> void set(int n, T value) {};
-template <class T, class U> void set_(int n, T value, U& list) { list.assign(n, value); };
+template <class T> void set(int n, T value) {}
+template <class T, class U> void set_(int n, T value, U& list) { list.assign(n, value); }
 template <class T, class U, class... W> void set(int n, T value, U& head, W&... tail) {
-    set_(n, value, head); set(n, value, tail...); };
+    set_(n, value, head); set(n, value, tail...); }
 
 // ------------------ SOLVE ------------------
 
